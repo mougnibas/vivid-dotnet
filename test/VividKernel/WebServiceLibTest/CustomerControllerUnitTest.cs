c907784a95474a5a956666154992b58bd413fff4
Yoann MOUGNIBAS <yoann@mougnibas.fr>
2025-08-15 23:43:38 +0200

    feat(kernel): add tests

---
+++ test/VividKernel/WebServiceLibTest/CustomerControllerUnitTest.cs
@@ -0,0 +1,92 @@
+﻿// Version 3, 19 November 2007
+//
+// Copyright (C) 2007 Free Software Foundation, Inc. <https://fsf.org/>
+// Everyone is permitted to copy and distribute verbatim copies
+// of this license document, but changing it is not allowed.
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Vivid.Kernel.DataAccessService;
+using Vivid.Kernel.DataAccessService.InMemory;
+using Vivid.Kernel.Service;
+using Vivid.Kernel.Service.Core;
+using Vivid.Kernel.WebserviceLib.Controllers;
+
+namespace VividTest.VividKernel.WebserviceLibTest;
+
+/// <summary>
+/// Unit tests of ``CustomerController`` class.
+/// </summary>
+[TestClass]
+public sealed class CustomerControllerUnitTest
+{
+    private CustomerController? _controller;
+
+    [TestInitialize]
+    public void Setup()
+    {
+        // Initialize the in-memory data access service and kernel service.
+        IDataAccessService dataAccessService = new DataAccessServiceInMemory();
+        IKernelService kernelService = new KernelServiceCore(dataAccessService);
+
+        // Populate initial data
+        dataAccessService.AddCustomer(new Customer { Id = "my-id", Secret = "my-secret" });
+        dataAccessService.AddCustomer(new Customer { Id = "my-id-2", Secret = "my-secret-2" });
+
+        // Initialize the customer controller.
+        _controller = new CustomerController(kernelService);
+    }
+
+    [TestMethod]
+    public void SendPostToCustomerWithJsonShouldReturnThisNewCustomer()
+    {
+        // Arrange.
+        Customer expected = new Customer { Id = "my-id-3", Secret = "my-secret-3" };
+
+        // Act.
+        Customer? actual = _controller?.PostCustomer(expected).Value;
+
+        // Assert.
+        Assert.AreEqual(expected, actual);
+    }
+
+    [TestMethod]
+    public void SendPostToCustomerShouldReturnOneMoreCustomer()
+    {
+        // Arrange.
+        int initialCount = _controller?.GetCustomers()?.Value?.Length ?? 0;
+        int expected = initialCount + 1;
+
+        // Act.
+        _controller?.PostCustomer();
+        int actual = _controller?.GetCustomers()?.Value?.Length ?? 0;
+
+        // Assert.
+        Assert.AreEqual(expected, actual);
+    }
+
+    [TestMethod]
+    public void SendGetToCustomerWithUnknownIdShouldReturn404()
+    {
+        // Arrange.
+        Customer? expected = null;
+
+        // Act.
+        Customer? actual = _controller?.GetCustomer("my-id-not-found").Value;
+
+        // Assert.
+        Assert.AreEqual(expected, actual);
+    }
+
+    [TestMethod]
+    public void SendGetToCustomerWithMyIdParameterShouldReturnThatCustomer()
+    {
+        // Arrange.
+        Customer expected = new Customer { Id = "my-id", Secret = "my-secret" };
+
+        // Act.
+        Customer? actual = _controller?.GetCustomer("my-id").Value;
+
+        // Assert.
+        Assert.AreEqual(expected, actual);
+    }
+}
